d6ac91601caec303a76bafcc7c8f3719a361ce52
Robson Gomes <nosbor84@gmail.com>
2025-01-05 08:54:44 -0300

    parte 01

---
+++ 01/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+var (
+	screenWidth  = 640
+	screenHeight = 480
+)
+
+type Game struct {
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello ebitengine")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello Ebitengine")
+	game := Game{}
+	if err := ebiten.RunGame(&game); err != nil {
+		panic(err)
+	}
+}
